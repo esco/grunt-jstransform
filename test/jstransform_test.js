223d33eb0ed8ec869f45ed6e3edb21f3507954fb
Esco Obong <menzoic@gmail.com>
2014-03-25 01:49:15 -0400

    add tests for class and arrowfunciton visitors

--- test/jstransform_test.js
+++ test/jstransform_test.js
@@ -27,22 +27,31 @@ exports.jstransform = {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
+  parentClass: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options');
-    var expected = grunt.file.read('test/expected/default_options');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    var actual = grunt.file.read('tmp/parentClass.js');
+    var expected = grunt.file.read('test/expected/parentClass.js');
+    test.equal(actual, expected, 'should transpile class to ES5.');
 
     test.done();
   },
-  custom_options: function(test) {
+  childClass: function(test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/custom_options');
-    var expected = grunt.file.read('test/expected/custom_options');
-    test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+    var actual = grunt.file.read('tmp/childClass.js');
+    var expected = grunt.file.read('test/expected/childClass.js');
+    test.equal(actual, expected, 'should transpile child class to ES5');
 
     test.done();
   },
+  arrowFunctions: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read('tmp/arrowFunction.js');
+    var expected = grunt.file.read('test/expected/arrowFunction.js');
+    test.equal(actual, expected, 'should transpile child arrow functions to ES5');
+
+    test.done();
+  }
 };
