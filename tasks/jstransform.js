ac206204762ff661c65b2dc0835f358e7173cdef
Esco Obong <menzoic@gmail.com>
2014-04-07 10:43:40 -0400

    made task async and used highland streams

--- tasks/jstransform.js
+++ tasks/jstransform.js
@@ -11,55 +11,49 @@
 var jstransform = require('jstransform');
 var util = require('util');
 var chalk = require('chalk');
-var async = require('async');
+var _ = require('highland');
 
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
   grunt.registerMultiTask('jstransform', 'Grunt task for transpiling ES6 --> ES5 using Facebook\'s jstransform', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var visitors = [];
-    var options = this.options({
-      separator: ';'
-    });
-
-    visitors = options.visitors.reduce(function(prev, current){
+    var done = this.async();
+    var options = this.options({ separator: ';', visitors: [] });
+    var visitors = options.visitors.reduce(function(prev, current){
       var list = getVisitorList(current);
       return prev.concat(list);
     }, []);
+    var _transpile = _.curry(transpile, visitors);
+    var doneCount = 0;
+    var numFiles = this.files.length;
 
-    console.log('visitors --->', visitors);
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        var originalFile = grunt.file.read(filepath);
-        var transformedFileData = jstransform.transform(
-          visitors,
-          originalFile
-        );
-        return transformedFileData.code;
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+    this.files.forEach(function(fileData){
+      _(fileData.orig.src)
+        .map(checkExist)
+        .map(grunt.file.read)
+        .map(_transpile)
+        .toArray(function(transpiled){
+          var output = transpiled.join(grunt.util.normalizelf(options.separator));
+          grunt.file.write(fileData.dest, output);
+          grunt.log.writeln('File ' + chalk.cyan(fileData.dest) + ' created.');
+          if (++doneCount === numFiles) {
+            done();
+          }
+        });
     });
   });
+
+  function checkExist(filepath) {
+    if (!grunt.file.exists(filepath)) {
+      grunt.log.warn('Source file "' + filepath + '" not found.');
+    }
+    return filepath;
+  }
+
+  function transpile(visitors, source){
+    var transformedFileData = jstransform.transform(visitors, source);
+    return transformedFileData.code;
+  }
 
   function getVisitorList(visitorType) {
     var modulePath = util.format('jstransform/visitors/es6-%s-visitors', visitorType);
